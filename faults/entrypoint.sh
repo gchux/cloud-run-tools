a06231edab56dc817ae5970fafe3b0020a13960a
javalladares <javalladares@google.com>
2025-02-12 05:14:09 +0000

    [FIX]: x namespace for installed tools

--- faults/entrypoint.sh
+++ faults/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ -z "${FAULTS_CONFIG}" ]]; then
-  export FAULTS_CONFIG='/faults-generator.properties'
+  export FAULTS_CONFIG='/x/faults-generator.properties'
 fi
 
 set -x
 
-exec java -cp /app \
+exec java -cp /x/rest_app \
   dev.chux.gcp.crun.faults.FaultsApp \
   --config=${FAULTS_CONFIG}
