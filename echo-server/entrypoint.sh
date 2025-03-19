bcaa50c1b0a723533345fec1737ea2a8988fe745
javalladares <javalladares@google.com>
2025-03-19 19:41:28 +0000

    [FIX]: echo server build

--- echo-server/entrypoint.sh
+++ echo-server/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ -z "${FAULTS_CONFIG}" ]]; then
-  export FAULTS_CONFIG='/x/echo-server.properties'
+if [[ -z "${ECHO_CONFIG}" ]]; then
+  export ECHO_CONFIG='/x/echo-server.properties'
 fi
 
 set -x
 
-exec java -cp /x/rest_app \
-  dev.chux.gcp.crun.echo.EchoServerApp \
-  --config=${FAULTS_CONFIG}
+exec java \
+  -Decho.log.level=${ECHO_LOG_LEVEL:-INFO} \
+  -cp /x/app dev.chux.gcp.crun.echo.EchoServerApp \
+  --config=${ECHO_CONFIG}
